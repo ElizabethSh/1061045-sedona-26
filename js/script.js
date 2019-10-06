388e87a063ee5531419d1b3a5b1cf5d1766ffe56
Lisa <liza.shoshkina@gmail.com>
2019-10-06 10:46:57 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var button = document.querySelector(".button-search");
+var popup = document.querySelector(".booking-form");
+
+var checkInDate = popup.querySelector("[name=check-in-date]");
+var checkOutDate = popup.querySelector("[name=check-out-date]");
+var adultsAmount = popup.querySelector("[name=adults-amount]");
+var childrenAmount = popup.querySelector("[name=children-amount]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("adultsAmount");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-close");
+  popup.classList.remove("modal-error");
+});
+
+popup.addEventListener("submit", function(evt) {
+  if (!checkInDate.value || !checkOutDate.value || !adultsAmount.value || !childrenAmount.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adultsAmount", adultsAmount.value);
+    }
+  }
+});
